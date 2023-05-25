8710122201bcd676173d04f28aa1c9ac9c6806b8
ritasm1704 <ririchia.sm1704@mail.ru>
2023-05-25 20:08:38 +0300

    queue test

--- TestTask/libs/threads/test/queue_test.cpp
+++ TestTask/libs/threads/test/queue_test.cpp
@@ -2,9 +2,34 @@
 #include "threads/myQueue.h"
 #include "threads/dataMsg.h"
 
-TEST(QueueTest, Creating) {
+class QueueTest : public ::testing::Test {
 
-    MyQueue<DataMsg> mq;
+    void SetUp() override {
+
+        q1_.put(0);
+
+        q2_.put(1);
+        q2_.put(2);
+    }
+
+protected:
+    MyQueue<int> q0_;
+    MyQueue<int> q1_;
+    MyQueue<int> q2_;
+};
+
+
+TEST_F(QueueTest, isEmptyTest) {
+
+    EXPECT_EQ(q0_.isEmpty(), true);
+    EXPECT_EQ(q1_.isEmpty(), false);
+    EXPECT_EQ(q2_.isEmpty(), false);
+}
+
+TEST_F(QueueTest, getValueTest) {
+
+    EXPECT_EQ(q1_.getValue(), 0);
+    EXPECT_EQ(q2_.getValue(), 1);
 }
 
 
